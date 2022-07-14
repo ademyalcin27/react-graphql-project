6cd43ca70565a785bc8e6ed67b704c3c98f7bfd4
Adem Yalçın <ademyalcin27@gmail.com>
2022-07-14 13:37:48 +0300

    İnitial commit

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import "reflect-metadata";
+import { PostResolver } from './resolvers/post';
+import { HelloResolver } from './resolvers/hello';
+import { MikroORM } from '@mikro-orm/core';
+import mikroOrmConfig from './mikro-orm.config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express'
+import { buildSchema } from 'type-graphql';
+
+const main = async () => {
+    const orm = await MikroORM.init(mikroOrmConfig);
+    await orm.getMigrator().up();
+    const app = express();
+    
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver, PostResolver],
+            validate: false,
+        }),
+        context: () => ({ em: orm.em })
+    })
+    await apolloServer.start();
+    apolloServer.applyMiddleware({app});
+    app.listen(4000, () => {
+        console.log('server started on localhost:4000')
+    });
+}
+main().catch((err) => {
+    console.log(err);
+})
