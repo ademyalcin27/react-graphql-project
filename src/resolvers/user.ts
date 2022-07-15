67167d45b2167939d6010e85a35513e0ebc433f0
Adem Yalçın <ademyalcin27@gmail.com>
2022-07-15 17:08:49 +0300

    adds login

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,8 +1,8 @@
 import { MyContext } from 'src/types';
 import "reflect-metadata";
-import { Arg, Ctx, Field, InputType, Mutation, Resolver } from "type-graphql";
+import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Resolver } from "type-graphql";
 import { User } from '../entities/User';
-import { hash } from 'argon2';
+import { hash, verify } from 'argon2';
 
 
 @InputType() 
@@ -13,13 +13,44 @@ class UsernamePasswordInput {
     password: string
 }
 
+@ObjectType() 
+class FieldError {
+    @Field()
+    field: string;
+    @Field() 
+    message: string
+}
+
+@ObjectType() 
+class UserResponse {
+    @Field(() => [FieldError], {nullable: true})
+    errors?: FieldError[]
+
+    @Field(() => User, {nullable: true})
+    user?: User
+}
+
 @Resolver()
 export class UserResolver {
-    @Mutation(() => User)
+    @Mutation(() => UserResponse)
     async register(
         @Arg('options')  options: UsernamePasswordInput,
         @Ctx() {em}: MyContext
-    ) {
+    ): Promise<UserResponse> {
+        if(options.username.length <=2) {
+            return {
+                errors: [
+                    {field: 'username', message: 'length must be greater than 2'}
+                ]
+            }
+        }
+        if(options.password.length <=3) {
+            return {
+                errors: [
+                    {field: 'password', message: 'length must be greater than 3'}
+                ]
+            }
+        }
         const hashedPassword = await hash(options.password)
         const user = em.fork({}).create(User, {
             username: options.username, 
@@ -27,5 +58,28 @@ export class UserResolver {
         });
         await em.persistAndFlush(user);
         return user;
+    }
+    @Mutation(() => UserResponse)
+    async login(
+        @Arg('options')  options: UsernamePasswordInput,
+        @Ctx() {em}: MyContext
+    ): Promise<UserResponse> {
+        const user = await em.findOne(User, { username: options.username});
+        if (!user) {
+            return {
+                errors: [
+                    { field: 'username', message: 'That username doesnt exist'}
+                ]
+            }
+        }
+        const valid = await verify(user.password,options.password)
+        if (!valid) {
+            return {
+                errors: [
+                    { field: 'password', message: 'incorrect password'}
+                ]
+            }
+        }
+        return { user };
     }
 }
